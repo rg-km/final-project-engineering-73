9b06ccde84fe571e0fd74a0c1071d231b2693fdf
IlhamRasyid111 <m.ilhamridha27@gmail.com>
2022-06-16 22:55:05 +0800

    style: typo content text di Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -12,7 +12,7 @@ function Home(){
        <div className="px-4 pt-5 my-5 text-center border-bottom hero">
         <h1 className="display-4 fw-bold">Selamat Datang Di GURUKU</h1>
         <div className="col-lg-6 mx-auto">
-          <p className="lead mb-4">Guruku adalah platfrom pembelajarn untuk segala jenjang pendidikan disini kamu dapat menemukan guru yang terpercaya kredibilitasnya.</p>
+          <p className="lead mb-4">Guruku adalah platfrom pembelajaran untuk segala jenjang pendidikan disini kamu dapat menemukan guru yang terpercaya kredibilitasnya.</p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
             <button type="button" className="btn btn-info btn-lg px-4 me-sm-3 text-white">Baca Selengkapnya </button>
           </div>
