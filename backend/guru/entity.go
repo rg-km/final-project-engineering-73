5878c5aa89615b6b83cc9e0272836a2922598408
Rika An Rokhim <rikaanrokhim1@gmail.com>
2022-06-16 14:38:30 +0700

    feat: login for user guru

---
+++ backend/guru/entity.go
@@ -0,0 +1,22 @@
+package guru
+
+// struct guru
+type Guru struct {
+	Id_guru      int
+	Username     string
+	Password     string
+	Nama_lengkap string
+	Gender       string
+	Usia         int
+	Alamat       string
+	Email        string
+	No_tlp       int
+	Tarif        int
+	No_rek       string
+	Id_mapel     int
+	Id_kelas     int
+	Id_role      int
+	Mapel        string
+	Kelas        string
+	Role         string
+}
