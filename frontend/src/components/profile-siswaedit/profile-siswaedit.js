3d7e08a7b91577fadb5dfdc977a9e1679b8e1684
IlhamRasyid111 <m.ilhamridha27@gmail.com>
2022-06-23 17:07:10 +0800

    repactor: add navbar to profile-siswaedit

--- frontend/src/components/profile-siswaedit/profile-siswaedit.js
+++ frontend/src/components/profile-siswaedit/profile-siswaedit.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
 import  './profile-siswaedit.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import {BrowserRouter ,Link } from "react-router-dom";
+import logo from '../navbar/guruku.png';
 
 function Profilesiswaedit() {
     return (
+        <>
+        <div>
+          <Navbar bg="light text-dark p-3" expand="lg">
+              <Container>
+              <Navbar.Brand href="/" className='text-white'><img src={logo} alt="" width="100" height="50"/></Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                  < input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                  </Nav>
+                <Nav>
+                 <Link to="/kegiatanku" className="nav-link  ">Kegiatanku</Link>
+                 <Link to="/cari" className="nav-link ">Cari Guru</Link>
+                 <Link to="/username" className="nav-link  ms-5">Username</Link> 
+                </Nav>
+              </Navbar.Collapse>
+              </Container>
+          </Navbar>
+        </div>
+        
         <form>
             <div class="row mb-3">
                 <label for="inputNama" class="col-sm-2 col-form-label">Nama</label>
@@ -49,6 +73,7 @@ function Profilesiswaedit() {
             </div>
             <button type="submit" class="btn btn-primary">Simpan</button>
         </form>
+        </>
         );
 }
 export default Profilesiswaedit;
