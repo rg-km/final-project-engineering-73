488c734aa08ea0368548ee15ab03766556f8ab64
febrian <febyianandy@gmai.com>
2022-06-27 06:10:22 +0700

    update cari-guru

--- frontend/src/components/cari-guru/CariGuru.jsx
+++ frontend/src/components/cari-guru/CariGuru.jsx
@@ -4,6 +4,7 @@ import {Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 import {BrowserRouter ,Link } from "react-router-dom";
 import logo from '../navbar/guruku.png';
 import NavbarSiswa from '../navbar/NavbarSiswa';
+import img from '../profile-pengajar/team-male.jpeg'
 const CariGuru = ()=>{
   return(
     <>
@@ -13,68 +14,33 @@ const CariGuru = ()=>{
           <div className="row">
             <div className="col-md-4">
               <div className="card mt-5 mb-5" >
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src={img} class="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">Nama Pengajar</h5>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#"className="btn btn-primary">Go somewhere</a>
+                  <a href="#"className="btn btn-primary">Profile</a>
                 </div>
               </div>
             </div>
 
             <div className="col-md-4">
               <div className="card mt-5 mb-5" >
-                <img src="..." class="card-img-top" alt="..."/>
+              <img src={img} class="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">Nama Pengajar</h5>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#"className="btn btn-primary">Go somewhere</a>
+                  <a href="#"className="btn btn-primary">Profile</a>
                 </div>
               </div>
             </div>
 
             <div className="col-md-4">
               <div className="card mt-5 mb-5" >
-                <img src="..." class="card-img-top" alt="..."/>
+              <img src={img} class="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">Nama Pengajar</h5>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#"className="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card mt-5 mb-5" >
-                <img src="..." class="card-img-top" alt="..."/>
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#"className="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="card mt-5 mb-5" >
-                <img src="..." class="card-img-top" alt="..."/>
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#"className="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="card mt-5 mb-5" >
-                <img src="..." class="card-img-top" alt="..."/>
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#"className="btn btn-primary">Go somewhere</a>
+                  <a href="#"className="btn btn-primary">Profile</a>
                 </div>
               </div>
             </div>
@@ -83,33 +49,68 @@ const CariGuru = ()=>{
           <div className="row">
             <div className="col-md-4">
               <div className="card mt-5 mb-5" >
-                <img src="..." class="card-img-top" alt="..."/>
+              <img src={img} class="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">Nama Pengajar</h5>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#"className="btn btn-primary">Go somewhere</a>
+                  <a href="#"className="btn btn-primary">Profile</a>
                 </div>
               </div>
             </div>
 
             <div className="col-md-4">
               <div className="card mt-5 mb-5" >
-                <img src="..." class="card-img-top" alt="..."/>
+              <img src={img} class="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">Nama Pengajar</h5>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#"className="btn btn-primary">Go somewhere</a>
+                  <a href="#"className="btn btn-primary">Profile</a>
                 </div>
               </div>
             </div>
 
             <div className="col-md-4">
               <div className="card mt-5 mb-5" >
-                <img src="..." class="card-img-top" alt="..."/>
+              <img src={img} class="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">Nama Pengajar</h5>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#"className="btn btn-primary">Go somewhere</a>
+                  <a href="#"className="btn btn-primary">Profile</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card mt-5 mb-5" >
+              <img src={img} class="card-img-top" alt="..."/>
+                <div className="card-body">
+                <h5 className="card-title">Nama Pengajar</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#"className="btn btn-primary">Profile</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card mt-5 mb-5" >
+              <img src={img} class="card-img-top" alt="..."/>
+                <div className="card-body">
+                <h5 className="card-title">Nama Pengajar</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#"className="btn btn-primary">Profile</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card mt-5 mb-5" >
+              <img src={img} class="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Nama Pengajar</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#"className="btn btn-primary">Profile</a>
                 </div>
               </div>
             </div>
