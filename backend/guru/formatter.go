2fc7d3c73ab32150415617b2667787473f25402b
Rika An Rokhim <rikaanrokhim1@gmail.com>
2022-06-27 16:44:04 +0700

    feat: update guru profile

--- backend/guru/formatter.go
+++ backend/guru/formatter.go
@@ -42,6 +42,34 @@ func FormatGuru(input Guru) guruFormat {
 	}
 }
 
+type updateGuruFormat struct {
+	Username     string `json:"username"`
+	Password     string `json:"password"`
+	Nama_lengkap string `json:"nama_lengkap"`
+	Gender       string `json:"gender"`
+	Usia         int    `json:"usia"`
+	Alamat       string `json:"alamat"`
+	Email        string `json:"email"`
+	No_tlp       int    `json:"no_tlp"`
+	Tarif        int    `json:"tarif"`
+	No_rek       string `json:"no_rek"`
+}
+
+func FormatUpdateGuru(input UpdateGuru) updateGuruFormat {
+	return updateGuruFormat{
+		Username:     input.Username,
+		Password:     input.Password,
+		Nama_lengkap: input.Nama_lengkap,
+		Gender:       input.Gender,
+		Usia:         input.Usia,
+		Alamat:       input.Alamat,
+		Email:        input.Email,
+		No_tlp:       input.No_tlp,
+		Tarif:        input.Tarif,
+		No_rek:       input.No_rek,
+	}
+}
+
 type FormatFindGuru struct {
 	Id_guru      int    `json:"id_guru"`
 	Username     string `json:"username"`
