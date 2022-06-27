ec724e4c7bf75eb2529033bd155ee0359b39001b
Muhammad Shabri rabbani <shabri@mhs.unsyiah.ac.id>
2022-06-27 16:51:01 +0700

    update profilePengajar page

--- frontend/src/components/profile-pengajar/ProfilePengajar.js
+++ frontend/src/components/profile-pengajar/ProfilePengajar.js
@@ -1,61 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import NavbarPengajar from "../../components/navbar/NavbarPengajar";
 import { Container,Row,Col,Form ,Button} from 'react-bootstrap';
-import DefaultUserPic from "./team-male.jpeg";
+import profilePic from "./team-male.jpeg";
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 
-export default class ProfilePengajar extends Component {
-  
+function ProfilePengajar (){
+ 
+  const param = 1 ;
+  const [nama_lengkap, setnama_lengkap] = useState([])
+  const [usia, setusia] = useState([])
+  const [lulusan, setlulusan] = useState([])
+  const [pengajar, setpengajar] = useState([])
+  const [kelas, setkelas] = useState([])
+  const [telepon, settelepon] = useState([])
+  const [tarif, settarif] = useState([])
+  const [no_rekening, setno_rekening] = useState([])
 
-  constructor(props) {
-      super(props)
-
-      this.state = {
-          id_guru:this.props.id_guru,
-          username:this.props.username,
-          nama_lengkap:this.props.nama_lengkap,
-          gender:this.props.gender,
-          pengajar:this.props.id_mapel,
-          kelas:this.props.kelas,
-          telepon:this.props.no_tlp,
-          tarif:this.props.tarif,
-          rekening:this.props.no_rek
-      }
+  const fetchData = () => {
+    fetch(`http://localhost:8000/data.guru.`+ param)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setnama_lengkap(data[0].nama_lengkap)
+      setusia(data[0].usia)
+      setlulusan(data[0].lulusan)
+      setpengajar(data[0].pengajar)
+      setkelas(data[0].kelas)
+      settelepon(data[0].telepon)
+      settarif(data[0].tarif)
+      setno_rekening(data[0].no_rekening)
+    })
   }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-  fetchUserDetails=(id_guru)=>{
-      axios.get(" "+id_guru)
-        .then(res=>{
-            console.log(res)
-            this.setState({username:res.data.results[0].username});
-            this.setState({nama_lengkap:res.data.results[0].nama_lengkap});
-            this.setState({gender:res.data.results[0].gender});
-            this.setState({pengajar:res.data.results[0].pengajar});
-            this.setState({kelas:res.data.results[0].kelas});
-            this.setState({telepon:res.data.results[0].telepon});
-            this.setState({tarif:res.data.results[0].tarif});
-            this.setState({rekening:res.data.results[0].rekening});
-        })
-        .catch(err=>console.log(err))
-  }
-
-  changeProfileImage=(event)=>{
-      this.setState({uploadedFile:event.target.file[0]})
-  }
-
-  
-
-  render() {
-
-    if(this.state.profileImage){
-        var imagestr=this.state.profileImage;
-        imagestr = imagestr.replace("/","")
-        var profilePic=""+imagestr
-    }else{
-        profilePic=DefaultUserPic;
-    }
 
     return (
       <>
@@ -70,7 +52,7 @@ export default class ProfilePengajar extends Component {
             <Form.Group as={Row} className="mb-1" controlId="formRekening">
              <Form.Label column sm="5">No Rekening</Form.Label>
              <Col sm="7">
-             <Form.Control plaintext readOnly defaultValue={this.state.rekening} />
+             <Form.Control plaintext readOnly defaultValue={no_rekening} />
              </Col>
             </Form.Group>
             </Form>
@@ -84,49 +66,49 @@ export default class ProfilePengajar extends Component {
             <Form.Group as={Row} className="mb-3" controlId="formNama">
              <Form.Label column sm="2">Nama</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue={this.state.nama_lengkap} />
+             <Form.Control plaintext readOnly defaultValue={nama_lengkap} />
              </Col>
             </Form.Group>
             
             <Form.Group as={Row} className="mb-3" controlId="formUsia">
              <Form.Label column sm="2">Usia</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue={this.state.usia} />
+             <Form.Control plaintext readOnly defaultValue={usia} />
              </Col>
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3" controlId="formLulusan">
              <Form.Label column sm="2">Lulusan</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue={this.state.lulusan} />
+             <Form.Control plaintext readOnly defaultValue={lulusan} />
              </Col>
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3" controlId="formPengajar">
              <Form.Label column sm="2">Pengajar</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue={this.state.pengajar} />
+             <Form.Control plaintext readOnly defaultValue={pengajar} />
              </Col>
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3" controlId="formKelas">
              <Form.Label column sm="2">Kelas</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue={this.state.kelas} />
+             <Form.Control plaintext readOnly defaultValue={kelas} />
              </Col>
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3" controlId="formTelepon">
              <Form.Label column sm="2">Telepon</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue={this.state.telepon} />
+             <Form.Control plaintext readOnly defaultValue={telepon} />
              </Col>
             </Form.Group>
 
-            <Form.Group as={Row} className="mb-3" controlId="formDeskripsi">
-             <Form.Label column sm="2">Deskripsi</Form.Label>
+            <Form.Group as={Row} className="mb-3" controlId="formTarif">
+             <Form.Label column sm="2">Tarif</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue={this.state.deskripsi} />
+             <Form.Control plaintext readOnly defaultValue={tarif} />
              </Col>
             </Form.Group>
 
@@ -141,4 +123,6 @@ export default class ProfilePengajar extends Component {
       </>
     )
   }
-}
+
+  export default ProfilePengajar;
+
