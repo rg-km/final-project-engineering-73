72572d729361ffc3e939a1c2207d3d8597b7005a
IlhamRasyid111 <m.ilhamridha27@gmail.com>
2022-06-27 14:40:52 +0800

    refactor: file profile-siswa

--- frontend/src/components/profile-siswa/profile-siswa.js
+++ frontend/src/components/profile-siswa/profile-siswa.js
@@ -1,15 +1,62 @@
 import React, { Component } from "react";
 import NavbarSiswa from "../../components/navbar/NavbarSiswa";
 import { Container,Row,Col,Form ,Button} from 'react-bootstrap';
-import profilePic from "./team-male.jpeg";
+import DefaultUserPic from "./team-male.jpeg";
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
-function ProfileSiswa() {
+export default class ProfileSiswa extends Component {
+  
+  constructor(props) {
+      super(props)
 
-  return (
-    <>
+      this.state = {
+          id_siswa:this.props.id_siswa,
+          username:this.props.username,
+          password:this.props.password,
+          nama_lengkap:this.props.nama_lengkap,
+          usia:this.props.usia,
+          gender:this.props.gender,
+          alamat:this.props.alamat,
+          email:this.props.email,
+          telepon:this.props.no_tlp
+      }
+  }
+
+  fetchUserDetails=(id_siswa)=>{
+      axios.get(" "+id_siswa)
+        .then(res=>{
+            console.log(res)
+            this.setState({username:res.data.results[0].username});
+            this.setState({password:res.data.results[0].password});
+            this.setState({nama_lengkap:res.data.results[0].nama_lengkap});
+            this.setState({usia:res.data.results[0].usia});
+            this.setState({gender:res.data.results[0].gender});
+            this.setState({alamat:res.data.results[0].alamat});
+            this.setState({email:res.data.results[0].email});
+            this.setState({telepon:res.data.results[0].telepon});
+        })
+        .catch(err=>console.log(err))
+  }
+
+  changeProfileImage=(event)=>{
+      this.setState({uploadedFile:event.target.file[0]})
+  }
+
+
+  render() {
+
+    if(this.state.profileImage){
+        var imagestr=this.state.profileImage;
+        imagestr = imagestr.replace("/","")
+        var profilePic=""+imagestr
+    }else{
+        profilePic=DefaultUserPic;
+    }
+
+    return (
+      <>
       <NavbarSiswa />
-
       <Container>
         <Row>
           <Col >
@@ -21,60 +68,60 @@ function ProfileSiswa() {
           <br/>
             <h1>User Profile</h1>
             <Form className="mt-3">
-    
+
             <Form.Group as={Row} className="mb-3" controlId="formNama">
              <Form.Label column sm="2">Nama</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue="" />
+             <Form.Control plaintext readOnly defaultValue={this.state.nama_lengkap} />
+             </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formUsername">
+             <Form.Label column sm="2">Username</Form.Label>
+             <Col sm="10">
+             <Form.Control plaintext readOnly defaultValue={this.state.username} />
+             </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formPassword">
+             <Form.Label column sm="2">Password</Form.Label>
+             <Col sm="10">
+             <Form.Control plaintext readOnly defaultValue={this.state.password} />
              </Col>
             </Form.Group>
             
             <Form.Group as={Row} className="mb-3" controlId="formUsia">
              <Form.Label column sm="2">Usia</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue="" />
+             <Form.Control plaintext readOnly defaultValue={this.state.usia} />
              </Col>
             </Form.Group>
 
-            <Form.Group as={Row} className="mb-3" controlId="formSekolah">
-             <Form.Label column sm="2">Sekolah</Form.Label>
+            <Form.Group as={Row} className="mb-3" controlId="formGender">
+             <Form.Label column sm="2">Gender</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue="" />
-             </Col>
-            </Form.Group>
-
-            <Form.Group as={Row} className="mb-3" controlId="formKelas">
-             <Form.Label column sm="2">Kelas</Form.Label>
-             <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue="" />
-             </Col>
-            </Form.Group>
-
-            <Form.Group as={Row} className="mb-3" controlId="formHobby">
-             <Form.Label column sm="2">Hobby</Form.Label>
-             <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue="" />
+             <Form.Control plaintext readOnly defaultValue={this.state.gender} />
              </Col>
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3" controlId="formAlamat">
              <Form.Label column sm="2">Alamat</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue="" />
+             <Form.Control plaintext readOnly defaultValue={this.state.alamat} />
              </Col>
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3" controlId="formEmail">
              <Form.Label column sm="2">Email</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue="" />
+             <Form.Control plaintext readOnly defaultValue={this.state.email} />
              </Col>
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3" controlId="formTelepon">
              <Form.Label column sm="2">Telepon</Form.Label>
              <Col sm="10">
-             <Form.Control plaintext readOnly defaultValue="" />
+             <Form.Control plaintext readOnly defaultValue={this.state.telepon} />
              </Col>
             </Form.Group>
 
@@ -86,9 +133,7 @@ function ProfileSiswa() {
           </Col>
         </Row>
       </Container>
-    </>
-  )
-    
+      </>
+    )
+  }
 }
-
-export default ProfileSiswa;
